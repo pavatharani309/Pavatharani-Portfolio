eb63c4729a038e511e5512962f40d591b7a0f02f
pavatharani309 <141811047+pavatharani309@users.noreply.github.com>
2026-01-15 20:20:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    toggleBtn.textContent = "☀️";
+  } else {
+    toggleBtn.textContent = "🌙";
+  }
+});
